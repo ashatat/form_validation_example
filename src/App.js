ede8d5cb10b2b2517ff76a66993c3ecf5ebcfde6
Ahmed A. Shatat <a.shatat@hotmail.com>
2020-12-21 23:19:22 +0200

    add eslint setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Signup from "./pages/Signup/index";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route>404 NOT FOUND PAGE</Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
